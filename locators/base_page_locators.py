abe85d6a4fe04f7983e166fe525970e45d33ed12
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-05 13:22:57 +0300

    added comments

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators():
-
+    #кнопка "Конструктор" в шапке страницы
     HEADER_BUTTON_BUILDER = (By.XPATH, ".//p[text()='Конструктор']")
+    #кнопка "Лента Заказов" в шапке страницы
     HEADER_BUTTON_FEED = (By.XPATH, ".//p[text()='Лента Заказов']")
