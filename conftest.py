2ffd0d1a6a3e7df2ac2479c2690eed85549d223a
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-05 16:37:44 +0300

    added fixture driver_with_auth

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from pages.login_page import LoginPage
+from data import UserLoginData as ULData
 
 
 #запуск браузера
@@ -11,6 +13,23 @@ def driver(request):
         driver = webdriver.Firefox()
     elif request.param == 'chrome':
         driver = webdriver.Chrome()
+
+    yield driver
+
+    driver.quit()
+
+#запуск браузера с авторизацией
+@pytest.fixture(scope='function', params=['firefox', 'chrome'])
+def driver_with_auth(request):
+    driver = None
+
+    if request.param == 'firefox':
+        driver = webdriver.Firefox()
+    elif request.param == 'chrome':
+        driver = webdriver.Chrome()
+
+    l_page = LoginPage(driver)
+    l_page.user_login(ULData.EMAIL, ULData.PASSWORD)
 
     yield driver
 
