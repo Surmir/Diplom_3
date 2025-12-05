0fd56ea07a760faa0f9321b21034adedaf32f866
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-05 17:40:24 +0300

    added class MainPage

---
+++ pages/main_page.py
@@ -0,0 +1,52 @@
+from pages.base_page import BasePage
+from locators.base_page_locators import BasePageLocators as BPLocs
+from locators.main_page_locators import MainPageLocators as MPLocs
+from urls import Url
+import allure
+
+
+class MainPage(BasePage):
+
+    @allure.step('Открываем главную страницу')
+    def open_main_page(self):
+        self.go_to_page(Url.MAIN_PAGE)
+
+    @allure.step('Ожидание загрузки главной страницы')
+    def wait_load_main_page(self):
+        self.wait_for_visibility_element(MPLocs.MAIN_PAGE_HEADER)
+
+    @allure.step('Нажимаем на кнопку "Лента Заказов" в шапке страницы')
+    def click_button_feed(self):
+        self.click_on_element(BPLocs.HEADER_BUTTON_FEED)
+
+    @allure.step('Нажимаем на ингредиент')
+    def click_ingredient(self):
+        self.click_on_element(MPLocs.INGREDIENT)
+
+    @allure.step('Ожидаем появления окна "Детали ингредиента"')
+    def wait_load_window_order_details(self):
+        self.wait_for_visibility_element(MPLocs.WINDOW_INGR_DETAILS)
+
+    @allure.step('Нажимаем на кнопку "Х" в окне "Детали ингредиента"')
+    def click_button_x(self):
+        self.click_on_element(MPLocs.BUTTON_CLOSE_X)
+
+    @allure.step('Добавляем ингредиент в заказ')
+    def add_ingredient_to_order(self):
+        self.drag_and_drop_element(MPLocs.INGREDIENT, MPLocs.BURGER_CONSTRUCTOR)
+
+    @allure.step('Проверяем счетчик ингредиента')
+    def check_counter_ingredient(self):
+        return self.get_text(MPLocs.COUNTER_INGREDIENT)
+    
+    @allure.step('Нажимаем на кнопку "Оформить заказ"')
+    def click_button_create_order(self):
+        self.click_on_element(MPLocs.BUTTON_CREATE_ORDER)
+
+    @allure.step('Ожидаем появления окна id заказа')
+    def wait_load_window_order_details(self):
+        self.wait_for_visibility_element(MPLocs.ID_ORDER, 3)
+
+    @allure.step('Получаем id заказа')
+    def get_id_order(self):
+        return self.get_text(MPLocs.ID_ORDER)
