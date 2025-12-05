3d1639d7289d1028914252052c81e94c1c1a4613
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-05 15:52:09 +0300

    renamed BUTTON_CREATE_ORDER/added ID_ORDER in class MainPageLocators

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -7,7 +7,9 @@ class MainPageLocators():
     #кнопка'Войти в аккаунт'
     BUTTON_ENTER_ACCAUNT = (By.XPATH, ".//button[text()='Войти в аккаунт']")
     #кнопка "Оформить заказ"(появляется после авторизации)
-    BUTTON_ENTER_ACCAUNT = (By.XPATH, ".//button[text()='Оформить заказ']")
+    BUTTON_CREATE_ORDER = (By.XPATH, ".//button[text()='Оформить заказ']")
+    #идентификатор заказа
+    ID_ORDER = (By.XPATH, ".//h2[@class='Modal_modal__title_shadow__3ikwq Modal_modal__title__2L34m text text_type_digits-large mb-8']")
     #ингредиент "Флюоресцентная булка R2-D3"
     INGREDIENT = (By.XPATH, ".//p[text()='Флюоресцентная булка R2-D3']")
     #окно "Детали ингредиента"
