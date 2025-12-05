f9cf80ad5ecc886cd8d3deaa0c2201917aabdea0
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-05 15:22:44 +0300

    added metod drag_and_drop_element

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import allure
 
 
@@ -20,18 +21,6 @@ class BasePage():
     def wait_for_visibility_element(self, element, wait_time=5):
         WebDriverWait(self.driver, wait_time).until(EC.visibility_of_element_located(element))
 
-    @allure.step('Ожидаем открытия второго окна')
-    def wait_new_window_is_opened(self, wait_time=5):
-        WebDriverWait(self.driver, wait_time).until(EC.number_of_windows_to_be(2))
-
-    @allure.step('Переходим в новое окно')
-    def go_to_new_window(self):
-        original_window = self.driver.current_window_handle
-        for window_handle in self.driver.window_handles:
-            if window_handle != original_window:
-                self.driver.switch_to.window(window_handle)
-                break
-
     @allure.step('Нажимаем на элемент страницы')
     def click_on_element(self, element):
         element = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable(element))
@@ -49,3 +38,10 @@ class BasePage():
     @allure.step('Заполняем поле({placeholder})')
     def set_data(self, placeholder, data):
         self.driver.find_element(*placeholder).send_keys(data)
+
+    @allure.step('Перетащить элемент на целевой элемент')
+    def drag_and_drop_element(self, draggable_element, place_element):
+        actions = ActionChains(self.driver)
+        source_element = self.driver.find_element(*draggable_element)
+        target_element = self.driver.find_element(*place_element)
+        actions.drag_and_drop(source_element, target_element).perform()
