9bbb75168d776318307f05a65b7cf43ae43ffced
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-07 01:07:59 +0300

    added class TestWaitTime

--- data.py
+++ data.py
@@ -2,3 +2,11 @@ class UserLoginData():
 
     EMAIL = "greog@yandex.ru"
     PASSWORD = "576567rt"
+
+class TestWaitTime():
+
+    PAGE = 5
+    WINDOW = 5
+    VISIBLE = 5
+    INVISIBLE = 5
+    CLICKBLE = 5
