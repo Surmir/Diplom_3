a0d3bcf3c796a0e9ce711a13bd48833b1a93525d
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-06 16:49:44 +0300

    added fixture driver_main_page

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 from data import UserLoginData as ULData
 
 
@@ -13,6 +14,23 @@ def driver(request):
         driver = webdriver.Firefox()
     elif request.param == 'chrome':
         driver = webdriver.Chrome()
+
+    yield driver
+
+    driver.quit()
+
+#запуск браузера с переходом на главную страницу
+@pytest.fixture(scope='function', params=['firefox', 'chrome'])
+def driver_main_page(request):
+    driver = None
+
+    if request.param == 'firefox':
+        driver = webdriver.Firefox()
+    elif request.param == 'chrome':
+        driver = webdriver.Chrome()
+
+    m_page = MainPage(driver)
+    m_page.wait_load_main_page(7)
 
     yield driver
 
